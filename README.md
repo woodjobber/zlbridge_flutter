9a4a1400c940e256ba6e3ae2f1021b733ec17dce
soer <474436731@qq.com>
2023-10-26 15:24:57 +0800

    null_safe

---
+++ README.md
@@ -0,0 +1,116 @@
+
+## demo
+下载包到本地，可选择android或者ios模拟器运行查看效果图
+## 说明
+ZLBridge-flutter是为原生(android,ios)webview与JS提供数据交互更简单方便的一个小插件工具，其数据交互的原理iOS是基于[userContentController addScriptMessageHandler]以及android是基于webView.addJavascriptInterface接口执行js代码为window初始化一个zlbridge对象，后续数据交互都是通过window.zlbridge对象来共享。ZLBridge-flutter并不提供webview页面展示，只负责处理原生与js数据交互，可配合webview_flutter，flutter_webview_plugin等等三方库使用，可直接使用demo里封装好的webview_flutter_bridge,flutter_webview_plugin_bridge类也可自行创建。目前支持平台有
+<br/>[ZLBridge-iOS](https://github.com/FPJack/ZLBridge-iOS)
+<br/>[ZLBridge-Android](https://github.com/FPJack/ZLBridge-Android)
+<br/>[ZLBridge-JS](https://github.com/FPJack/ZLBridge-JS)
+<br/>[ZLBridge-flutter](https://github.com/FPJack/ZLBridge-flutter)
+<br/>[ZLBridge-RN](https://github.com/FPJack/ZLBridge-RN)
+
+
+## 安装
+```ruby
+dependencies:
+  zlbridge_flutter:
+    git:
+      url: https://github.com/FPJack/ZLBridge-flutter
+      path: zlbridge_flutter
+```
+## 初始化
+
+### bridge
+```Dart
+ ZLBridge bridge = ZLBridge();
+ //调用对应三方库执行原生js的API接口，
+ //flutter_webview_plugin:flutterWebViewPlugin.evalJavascript(js);
+ //webview_flutter:webVC.evaluateJavascript(js);
+ bridge.evaluateJavascriptAction((js) => webVC.evaluateJavascript(js));
+```
+### JavascriptChannel
+```Dart
+//用对应三方库的JavascriptChannel添加ZLBridge的channelName，以及回调成功时调用bridge.handleJSMessage(message),bridge的registHandler就能接受相对应注册的事件通知
+JavascriptChannel(
+        name: ZLBridge.channelName,
+        onMessageReceived: (JavascriptMessage message) {
+          bridge.handleJSMessage(message.message);
+});
+```
+
+## window.zlbridge初始化(可选本地原生注入初始化，也可以由H5远程导入初始化)
+原生
+```Dart
+  //！！！监听webview加载完成注入本地JS代码
+  bridge.injectLocalJS();
+```
+或者H5
+```JavaScript
+//初始化完成后也可通过window.zlbridge拿zlbridge对象,详细请查看ZLBridge-JS
+ var zlbridge = require('zlbridge')
+```
+
+## 原生与JS交互
+
+### JS调用原生test事件
+
+#### 无参数
+```JavaScript
+window.zlbridge.call('test',(arg) => {
+
+});
+```
+#### 有参数参数
+```JavaScript
+window.zlbridge.call('test',{key:"value"},(arg) => {
+
+});
+```
+#### 原生注册test事件
+```Dart
+bridge.registHandler('test', (obj, callback){
+	  //true：jS调用一次test事件只能接受原生一次传值，false：JS一次事件可接受多次传值
+      callback(obj,true);
+});
+```
+
+### 原生调用js
+
+#### 原生调用JS的jsMethod事件
+```Dart
+bridge.callHandler("jsMethod",args: ["原生信息"],completionHandler:(obj,error){
+                
+});
+```
+
+#### js注册jsMethod事件
+```JavaScript
+window.zlbridge.register("jsMethod",(arg) => {
+     return arg;
+});
+ ```
+或者
+ ```JavaScript
+window.zlbridge.registerWithCallback("jsMethod",(arg,callback) => {
+  //ture代表原生只能监听一次回调结果，false可以连续监听，默认传为true
+  callback(arg,true);
+});
+  ```
+
+## 通过本地注入JS脚本的，H5可以监听zlbridge初始化完成事件
+```JavaScript
+document.addEventListener('ZLBridgeInitReady', function() {
+    console.log('ZLBridge初始化完成');
+},false);
+  ```
+## ！！！！ flutter传给JS的值必须是可以json.encode转换的Object
+
+## Author
+
+范鹏, 2551412939@qq.com
+
+
+
+## License
+
+ZLBridge-flutter is available under the MIT license. See the LICENSE file for more info.
---
+++ lib/zlbridge.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+typedef JSCompletionHandler = void Function(Object? obj, String? error);
+typedef JSCallbackHandler = void Function(Object? obj, {bool end});
+typedef JSRegistHandler = void Function(
+    Object? obj, JSCallbackHandler? callback);
+typedef JSRegistUndefinedHandler = void Function(
+    String? name, Object? obj, JSCallbackHandler? callback);
+
+class ZLBridge {
+  static const String channelName = "ZLBridge";
+  Map<String, JSRegistHandler>? _registerHandlers;
+  Map<String, JSCompletionHandler>? _callHandlers;
+  JSRegistUndefinedHandler? _undefinedHandler;
+  Future<String> Function(String js)? _evaluateJavascriptFunc;
+  ZLBridge() {
+    _registerHandlers = <String, JSRegistHandler>{};
+    _callHandlers = <String, JSCompletionHandler>{};
+  }
+  void evaluateJavascriptAction(
+      Future<String> Function(String js) evaluateJavascriptFunc) {
+    _evaluateJavascriptFunc = evaluateJavascriptFunc;
+  }
+
+  void handleJSMessage(String message) {
+    if (_evaluateJavascriptFunc == null) return;
+    _ZLMsgBody msgBody = _ZLMsgBody.initWithMap(message);
+    String name = msgBody.name!;
+    String callID = msgBody.callID ?? '';
+    String error = msgBody.error ?? '';
+    bool end = msgBody.end ?? false;
+    String jsMethodId = msgBody.jsMethodId ?? '';
+    Object? body = msgBody.body;
+    if (callID.isNotEmpty) {
+      JSCompletionHandler? callHandler = _callHandlers?[callID];
+      if (callHandler != null) {
+        callHandler(body, error);
+        if (end) _callHandlers?.remove(callID);
+      }
+      return;
+    }
+    JSRegistHandler? registHandler = _registerHandlers?[name];
+    callback(Object? result, {bool end = true}) {
+      Map map = {};
+      map["end"] = end ? 1 : 0;
+      map["result"] = result;
+      String jsonResult = json.encode(map);
+      String js =
+          "window.zlbridge._nativeCallback('$jsMethodId','$jsonResult');";
+      _evaluateJavascriptFunc?.call(js);
+    }
+
+    registHandler != null
+        ? registHandler(body, callback)
+        : _undefinedHandler?.call(name, body, callback);
+  }
+
+  void injectLocalJS({void Function(Object? error)? callback}) async {
+    rootBundle
+        .loadString('packages/zlbridge_flutter/assets/zlbridge.js')
+        .then((value) {
+      _evaluateJavascriptFunc?.call(value).then((value) {
+        if (callback != null) callback(null);
+      }).catchError((onError) {
+        if (callback != null) callback(onError);
+      });
+    }).catchError((onError) {
+      callback?.call(onError);
+    });
+  }
+
+  void registerHandler(String methodName, JSRegistHandler registHandler) {
+    if (methodName.isEmpty) return;
+    _registerHandlers?[methodName] = registHandler;
+  }
+
+  void registerUndefinedHandler(JSRegistUndefinedHandler registHandler) {
+    _undefinedHandler = registHandler;
+  }
+
+  void removeRegisterHandlerWithMethodName(String name) {
+    _registerHandlers?.remove(name);
+  }
+
+  void removeAllRegisterHandler() {
+    _registerHandlers?.clear();
+  }
+
+  void hasNativeMethod(String name, void Function(bool exit)? callback) {
+    if (_evaluateJavascriptFunc == null) return;
+    if (callback == null) return;
+    if (name.isEmpty) callback(false);
+    String js = "window.zlbridge._hasNativeMethod('$name');";
+    _evaluateJavascriptFunc?.call(js).then((value) {
+      String v = value;
+      callback(v == "1");
+    }).catchError((onError) {
+      callback(false);
+    });
+  }
+
+  void callHandler(String methodName,
+      {List? args, JSCompletionHandler? completionHandler}) {
+    if (_evaluateJavascriptFunc == null) {
+      if (completionHandler != null) {
+        completionHandler(null, "请实现evaluateJavascriptAction");
+      }
+      return;
+    }
+    args = args ?? [];
+    Map map = <String, dynamic>{};
+    map["result"] = args;
+    String id0 = '';
+    if (completionHandler != null) {
+      int id = DateTime.now().millisecondsSinceEpoch;
+      id0 = "$id";
+      map["callID"] = id0;
+      _callHandlers?[id0] = completionHandler;
+    }
+    String jsonResult = json.encode(map);
+    String js = "window.zlbridge._nativeCall('$methodName','$jsonResult')";
+    _evaluateJavascriptFunc?.call(js).then((value) {}).catchError((onError) {
+      if (completionHandler != null) {
+        completionHandler(null, onError.toString());
+        _callHandlers?.remove(id0);
+      }
+    });
+  }
+
+  void destroyBridge() {
+    _registerHandlers?.clear();
+    _callHandlers?.clear();
+    _undefinedHandler = null;
+  }
+}
+
+class _ZLMsgBody {
+  String? name;
+  String? jsMethodId;
+  Object? body;
+  String? callID;
+  bool? end;
+  String? error;
+  _ZLMsgBody.initWithMap(String js) {
+    Map<String, dynamic> map = jsonDecode(js);
+    name = map["name"];
+    jsMethodId = map["jsMethodId"];
+    body = map["body"];
+    callID = map["callID"];
+    end = map["end"];
+    error = map["error"];
+  }
+}
